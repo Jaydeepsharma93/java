474c224be014e175a1e3e971596a28c798f25fc2
Jaydeepsharma93 <sharmajaydeep1515@gmail.com>
2023-08-31 17:06:22 +0530

    31/08/23

---
+++ src/Pattern2.java
@@ -0,0 +1,16 @@
+public class Pattern2{
+    public static void main(String[] args) {
+        System.out.println(" - - - ");
+        System.out.println("|      |");
+        System.out.println("j      |");
+        System.out.println("a      |");
+        System.out.println("y      |");
+        System.out.println("d      |");
+        System.out.println("e      |");
+        System.out.println("e      |");
+        System.out.println("p      |");
+        System.out.println("|      |");
+        System.out.println(" - - - ");
+    }
+
+}
