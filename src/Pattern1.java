c83357b7a86acd09bb098a461f1ad7f7d4bcb4b9
Jaydeep Sharma <143181361+Jaydeepsharma93@users.noreply.github.com>
2023-09-09 21:10:52 +0530

    Update Pattern1.java

--- src/Pattern1.java
+++ src/Pattern1.java
@@ -1,4 +1,4 @@
-public class Pattern1 {
+public class Pattern1 { 
     public static void main(String[] args) {
         System.out.println(" - - - - - - -");
         System.out.println("|             |");
