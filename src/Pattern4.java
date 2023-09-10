c148912bf5846777fdd28b13422197a07571a8e4
Jaydeep Sharma <143181361+Jaydeepsharma93@users.noreply.github.com>
2023-09-10 23:43:50 +0530

    Update Pattern4.java

--- src/Pattern4.java
+++ src/Pattern4.java
@@ -1,5 +1,5 @@
-public class Pattern4{
-    public static void main(String[] args){ 
+public class Pattern4 {
+    public static void main(String[] args) { 
         System.out.println("*");
         System.out.println("*");
         System.out.println("*                                     *  *");
